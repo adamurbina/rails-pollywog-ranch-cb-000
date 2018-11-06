f1854d185d7f8c0eecd1b970686d564989b876cc
Adam Urbina <adamurbina@gmail.com>
2018-11-06 23:54:47 +0000

    automatically backed up by learn

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -2,7 +2,7 @@ class TadpolesController < ApplicationController
   before_action :set_tadpole, only: [:show, :edit, :update, :destroy, :metamorphose]
 
   def metamorphose
-    @tadpole = find_by(id: params[:id])
+    
     @frog = Frog.create(name: @tadpole.name, color: @tadpole.color, pond: @tadpole.pond)
     binding.pry
   end
